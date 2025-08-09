82d379c222fd6b3f526800805cf77df0068eedbb
dorjoo <dorjoohover@gmail.com>
2025-08-09 18:51:11 +0800

    a

--- src/components/google.tsx
+++
@@ -1,7 +0,0 @@
-export default function ConnectGoogle() {
-  const connect = () => {
-    window.location.href = "http://localhost:4000/auth/google";
-  };
-
-  return <button onClick={connect}>Connect Google Calendar</button>;
-}
