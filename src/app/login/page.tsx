291e32315cc78ca3a5b866be3b8f58787694c459
a.k.a fl1p <bishu.hover@gmail.com>
2025-08-31 19:44:07 +0800

    login background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,8 @@ import { LoginPage } from "./components";
 
 const Page = async () => {
   return (
-    <section className="fixed top-0 left-0 custom-bg h-screen w-screen flex-center bg-cover bg-no-repeat p-4">
+    <section className="fixed top-0 left-0 custom-bg h-screen w-screen flex-center bg-cover bg-no-repeat p-4 bg-primary">
+      {/* <div className="custom-bg absolute top-0 left-0 size-full object-cover"></div> */}
       <div className="max-w-lg bg-white border backdrop-blur-md p-10 rounded-3xl space-y-10 w-full shadow-xl border-slate-600">
         <h1 className="text-2xl font-bold text-center">Zu Nailbar</h1>
         <LoginPage
