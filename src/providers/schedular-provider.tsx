5cd15ae7048646f02a3a21dab4eb32621c793869
dorjoo <dorjoohover@gmail.com>
2025-11-02 12:15:31 +0800

    a

--- src/providers/schedular-provider.tsx
+++ src/providers/schedular-provider.tsx
@@ -143,7 +143,7 @@ export const SchedulerProvider = ({
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
     const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     const weekNo = Math.ceil(
-      ((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7
+      ((d?.getTime() - yearStart?.getTime()) / 86400000 + 1) / 7
     );
     return weekNo;
   };
