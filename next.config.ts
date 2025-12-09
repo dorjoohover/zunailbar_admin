2dfe6e5866a83af14f8707e84712c3ed6ec5701a
dorjoo <dorjoohover@gmail.com>
2025-12-09 18:16:23 +0800

    a

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // reactStrictMode: false,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  reactStrictMode: false,
   images: {
     domains: ["api.zunailbar.mn", "localhost"],
   },
