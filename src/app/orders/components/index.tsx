72cbc7c9480db2db4cda3c35b986b703e91b97fc
dorjoo <dorjoohover@gmail.com>
2025-12-21 16:42:49 +0800

    a

--- src/app/orders/components/index.tsx
+++ src/app/orders/components/index.tsx
@@ -56,10 +56,10 @@ export const OrderPage = ({
   };
   const isFirstRender = useRef(true);
   useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
+    // if (isFirstRender.current) {
+    //   isFirstRender.current = false;
+    //   return;
+    // }
     refresh();
   }, [filter?.date, filter?.artist, filter?.branch, filter?.status]);
 
