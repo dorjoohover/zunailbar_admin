130f31cba3e2610410f1db302aa0429507ce7bf8
dorjoo <dorjoohover@gmail.com>
2026-03-03 09:23:07 +0800

    endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,14 +5,12 @@ export enum METHOD {
   patch = "PATCH",
   delete = "DELETE",
 }
-const BASE = 
-// process.env.API
-  // ? process.env.API?.endsWith("/")
-  //   ? process.env.API
-  //   : process.env.API + "/" 
-  // :
-   "http://localhost:5000/api/v1/";
-    // : "https://api.zunailbar.mn/api/v1/";
+const BASE = process.env.API
+  ? process.env.API?.endsWith("/")
+    ? process.env.API
+    : process.env.API + "/"
+  : //  "http://localhost:5000/api/v1/";
+    "https://api.zunailbar.mn/api/v1/";
 
 export enum Api {
   login = "login",
